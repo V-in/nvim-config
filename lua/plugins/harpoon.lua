b7d95fc0b284de53e49f6a4c31f9d6bbe0bfb4ed
V-in <vinicius_gbapb@icloud.com>
2025-09-06 17:42:45 -0300

    update harpoon to switch to existing window when applicable

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -32,7 +32,26 @@ return {
       table.insert(keys, {
         "<leader>" .. i,
         function()
-          require("harpoon"):list():select(i)
+          local harpoon = require("harpoon")
+          local harpoon_item = harpoon:list():get(i)
+
+          -- If the buffer is already open in another window
+          -- just switch to it, otherwise do the default behaviour
+          if harpoon_item then
+            local file_path = harpoon_item.value
+            for _, win_id in ipairs(vim.api.nvim_list_wins()) do
+              local buf_id = vim.api.nvim_win_get_buf(win_id)
+              local buf_name = vim.api.nvim_buf_get_name(buf_id)
+              local relative_buf = vim.fn.fnamemodify(buf_name, ":.")
+
+              if relative_buf == file_path or buf_name == file_path then
+                vim.api.nvim_set_current_win(win_id)
+                return
+              end
+            end
+          end
+
+          harpoon:list():select(i)
         end,
         desc = "Harpoon to File " .. i,
       })
